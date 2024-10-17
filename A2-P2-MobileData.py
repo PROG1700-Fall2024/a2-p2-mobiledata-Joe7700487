9884a1bfcb3bca2a5280ae3de67ddeef0c404ac0
joe <w0500154@nscc.ca>
2024-10-17 11:51:27 -0300

    P2 Complete

--- A2-P2-MobileData.py
+++ A2-P2-MobileData.py
@@ -10,14 +10,20 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    # get user input
+    mb = int(input("Enter data usage (Mb): "))
+    
+    # charge the correct rate based on the data usage
+    total = 118.0
+    if (mb <= 200):
+        total = 20
+    elif (mb > 200 and mb <= 500):
+        total = mb * 0.105
+    elif (mb > 500 and mb <= 1000):
+        total = mb * 110
 
-
-
-
-
-
-
-
+    # print final total
+    print("Total charge is ${0:.2f}".format(total))
     # YOUR CODE ENDS HERE
 
 main()
